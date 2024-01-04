6d93f3f9c9897e9b81b587a0e7b4eb9f9b3b4a45
Kanem <kanemdeng@email.com>
2024-01-04 07:23:00 -0800

    remove vite instead add CRA and Nest.Js

---
+++ react-app/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+
+  return (
+    <div className='container'>
+      <form>
+        <input type="text" />
+        <input type="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
